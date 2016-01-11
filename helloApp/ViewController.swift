7b5989a6cf60795d65fcb255f2267d29f22968e1
soumyajitz <2014soumyajit@gmail.com>
2016-01-11 02:08:23 -0600

    Button Response and Click events

--- helloApp/ViewController.swift
+++ helloApp/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textfield: UITextField!
+    @IBOutlet var label: UILabel!
+    @IBAction func click(sender: AnyObject) {
+        
+        print("Button Tapped")
+        label.text = textfield.text
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World my First App")
     }
 
     override func didReceiveMemoryWarning() {
